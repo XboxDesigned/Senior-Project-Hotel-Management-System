3765767c7973f0713e4f4f5aa0759696c2009170
Skyler <164084319+SkylerBytes@users.noreply.github.com>
2025-01-27 15:24:14 -0500

    Delete Hotel Mgmt/Website/homepage/Homepage Type directory

--- Hotel Mgmt/Website/homepage/Homepage Type/Admin/admin_homepage.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-
-<body>
-    <h2>You are an admin!</h2>
-</body>
--- Hotel Mgmt/Website/homepage/Homepage Type/Front Desk/frontdesk_homepage.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-
-<body>
-    <h2>You are front desk!</h2>
-</body>
