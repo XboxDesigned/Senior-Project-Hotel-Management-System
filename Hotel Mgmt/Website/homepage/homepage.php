39f8ca69cad4547c5d40e9807817757dc3e17449
Skyler <164084319+SkylerBytes@users.noreply.github.com>
2025-03-16 15:19:09 -0400

    Updated homepage to fix logout button

--- Hotel Mgmt/Website/homepage/homepage.php
+++ Hotel Mgmt/Website/homepage/homepage.php
@@ -1,19 +1,16 @@
 <?php
 // Start the session if not already started
-if (session_status() == PHP_SESSION_NONE) 
-{
+if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-if (!isset($_SESSION['user'])) 
-{
+if (!isset($_SESSION['user'])) {
     header('Location: ../login.php'); // Redirect to the login if not logged in
     exit();
 }
 
 // Check if the logout button is clicked
-if (isset($_POST['logout'])) 
-{
+if (isset($_POST['logout'])) {
     // Destroy the session
     session_destroy();
 
@@ -21,41 +18,34 @@ if (isset($_POST['logout']))
     header("Location: ../login.php");
     exit();
 }
-	$name = $_SESSION['user']['username'];
-	$role = $_SESSION['user']['role'];
+
+$name = $_SESSION['user']['username'];
+$role = $_SESSION['user']['role'];
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-        <style>
- 			td {
-       			padding: 10px 20px; /* Adjust space inside each cell, temporary */
-                width: 1200px;
-   			}
-		</style>
-	<header>
-		<title>Home Page</title>
-		<link rel="stylesheet" type="text/css" href="../inc/homepage_main.css">
-	</head>
-	<body>
-		<h2>Hello, <?php echo htmlspecialchars($name); ?></h2>
+    <title>Home Page</title>
+    <link rel="stylesheet" type="text/css" href="../inc/homepage_main.css">
+</head>
+<body>
 
-		<!-- Logout Button -->
-		<form method="post">
-			<button type="submit" name="logout" id="submit" class="logout-register-btn">LOGOUT</button><br>
-		</form>
 
-	</header>
-	
-	<h1>Home</h1>
-	
+    <header>
+        <h2>Hello, <?php echo htmlspecialchars($name); ?></h2>
+		
+        <form method="post" class="logout-button-container">
+            <button type="submit" name="logout" class="logout-button">LOGOUT</button>
+        </form>
+    </header>
+
+
+    <h1>Home</h1>
     <br>
-</body>
-</html>
 
-<?php
-
+    <?php
     switch ($role) {
         case 'front_desk':
             include_once('Homepage-Type/Front-Desk/frontdesk_homepage.php');
@@ -72,5 +62,7 @@ if (isset($_POST['logout']))
         default:
             echo "Invalid action!";
     }
-	
-?>
+    ?>
+</body>
+
+</html>
