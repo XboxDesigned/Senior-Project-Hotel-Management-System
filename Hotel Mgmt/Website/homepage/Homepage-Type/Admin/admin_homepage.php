96c5adb32cf117ee4cd28914afa1cb174fd8aabd
XboxDesigned <167447290+XboxDesigned@users.noreply.github.com>
2025-03-30 17:25:07 -0700

    Add files via upload

--- Hotel Mgmt/Website/homepage/Homepage-Type/Admin/admin_homepage.php
+++ Hotel Mgmt/Website/homepage/Homepage-Type/Admin/admin_homepage.php
@@ -6,10 +6,6 @@
         include('Homepage-Type/Admin/registerUser.php');
     }
 	
-	if (isset($_POST['night_audit'])) {
-		echo "<script>window.open('../../Website/inc/night_audit.php', '_blank');</script>";
-	}
-	
 	if (isset($_POST['select'])) {
         include('Homepage-Type/Admin/selectUsers.php');
     }
@@ -22,7 +18,50 @@
 	if (isset($_GET['update_id'])) {
 		include('Homepage-Type/admin/updateUsers.php');
 	}
+	
+	if (isset($_POST['home'])) {
+  include('../../Website/inc/dashboard.php');
+}
+
+else if (isset($_POST['rooms'])) {
+  include('Homepage-Type/Front-Desk/rooms.php');
+}
+
+else if (isset($_POST['cancellations'])) {
+  include('Homepage-Type/Front-Desk/cancellations.php');
+}
+
+else if (isset($_POST['guests'])) {
+  include('Homepage-Type/Front-Desk/guests.php');
+}
+
+else if (isset($_POST['maintenance'])) {
+  include('Homepage-Type/Front-Desk/page_template.php');
+}
+
+else if (isset($_POST['night_audit'])) {
+	include('Homepage-Type/Front-Desk/night_audit.php');
+  echo "<script>window.open('../../Website/inc/night_audit.php', '_blank');</script>";
+}
+
+else if (isset($_POST['book_room'])) {
+  include('Homepage-Type/Front-Desk/book_room.php');
+}
+
+else if (isset($_POST['check_in'])) {
+  include('Homepage-Type/Front-Desk/check_in.php');
+}
+
+else if (isset($_POST['check_out'])) {
+  include('Homepage-Type/Front-Desk/check_out.php');
+}
+
+else
+{
+	include('../../Website/inc/dashboard.php');
+}
 	?>
+
 
 
 <!DOCTYPE html>
@@ -41,19 +80,47 @@
     
     <form method="post">
         <button type="submit" name="select" id="side-buttons">View Users</button>
+		
+    </form>
+	<form method="post">
+        <button type="submit" name="home" id="side-buttons">Home</button>
     </form>
 	
 	<form method="post">
+        <button type="submit" name="rooms" id="side-buttons">Rooms</button>
+    </form>
+	
+	<form method="post">
+        <button type="submit" name="cancellations" id="side-buttons">Cancellations</button>
+    </form>
+	
+	<form method="post">
+        <button type="submit" name="guests" id="side-buttons">Guests</button>
+    </form>
+	</div>
+	
+
+	<div class="side-buttons-bottom">
+	<form method="post">
+        <button type="submit" name="maintenance" id="side-buttons">Maintenance</button>
+    </form>
+	
+    <form method="post">
         <button type="submit" name="night_audit" id="side-buttons">Night Audit</button>
     </form>
-	<br><br><br>
 	
 	<form method="post">
-        <button type="submit" name="placeholder" id="side-buttons">Function 2</button>
+        <button type="submit" name="book_room" id="side-buttons">Book Room</button>
     </form>
 	
 	<form method="post">
-        <button type="submit" name="placeholder" id="side-buttons">Function 3</button>
+        <button type="submit" name="check_in" id="side-buttons">Check In</button>
     </form>
+	
+	<form method="post">
+        <button type="submit" name="check_out" id="side-buttons">Check Out</button>
+    </form>
+	</div>
+	
 </body>
 </html>
